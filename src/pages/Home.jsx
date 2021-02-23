68e40cf91ad96954c584d04198ce0edd71237016
rojesh mote <rojesh123mote@gmail.com>
2021-02-23 20:02:02 +0545

    api added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Typography, Card } from "@material-ui/core";
 import "../styles/home.css";
+import axios from "axios";
 
 export default function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getDataFromApi();
+  }, []);
+
+  function getDataFromApi() {
+    axios
+      .get("http://192.168.1.100:5000/bank/")
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data);
+      })
+      .catch((error) => console.log(error));
+  }
+
   return (
     <div style={{ background: "#dadada", height: "100vh" }}>
       <Typography align="center" variant="h3">
@@ -16,26 +33,26 @@ export default function Home() {
           <div className="card-div">
             <p>
               <span style={{ color: "black" }}>Total</span>
-              <span style={{ color: "green" }}>:{100}</span>
+              <span style={{ color: "green" }}>:-{100}</span>
             </p>
             <p>
               <span style={{ color: "black" }}>Due</span>
-              <span style={{ color: "red" }}>:{30}</span>
+              <span style={{ color: "red" }}>:-{30}</span>
             </p>
           </div>
         </Card>
         <Card className="card-items">
           <Typography variant="h5" style={{ fontWeight: "bold" }}>
-            RojeshMiniBank Info:-
+            Rojesh MiniBank Info:-
           </Typography>
           <div className="card-div">
             <p>
               <span style={{ color: "black" }}>Total</span>
-              <span style={{ color: "green" }}>:{100}</span>
+              <span style={{ color: "green" }}>:-{100}</span>
             </p>
             <p>
               <span style={{ color: "black" }}>Due</span>
-              <span style={{ color: "red" }}>:{20}</span>
+              <span style={{ color: "red" }}>:-{20}</span>
             </p>
           </div>
         </Card>
@@ -45,11 +62,11 @@ export default function Home() {
         <div className="card-div">
           <p>
             <span style={{ color: "black" }}>Total</span>
-            <span style={{ color: "green" }}>:{200}</span>
+            <span style={{ color: "green" }}>:-{200}</span>
           </p>
           <p>
             <span style={{ color: "black" }}>Left</span>
-            <span style={{ color: "red" }}>:{50}</span>
+            <span style={{ color: "red" }}>:-{50}</span>
           </p>
         </div>
       </Card>
